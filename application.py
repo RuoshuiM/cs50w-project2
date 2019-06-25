b493c03c2423d5212148552281d10d9581d7a546
Ruoshui <ruoshuim@gmail.com>
2019-06-25 05:51:41 -0400

    Initial complete setup

---
+++ application.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask, render_template, url_for, send_from_directory
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+#  Only for dev mode backend: disable browser cache
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+socketio = SocketIO(app)
+
+# @app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.png'))
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.png', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+# Trying to add React to this project
+# https://stackoverflow.com/questions/54263609/adding-flask-server-to-create-react-app-default-app-404-on-dist-bundle-js
+
+app.run()
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/app.scss";
+
+class App extends React.Component {
+  render() {
+    return <div>Project2: TODO</div>;
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+);
